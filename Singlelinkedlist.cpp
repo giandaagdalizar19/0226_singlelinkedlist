bcf1f805c8253c236bb49316922a563ec9eda02b
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-04-21 11:09:58 +0700

    class linkedlist

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -9,4 +9,31 @@ public:
     Node *next;
 };
 
+class LinkedList
+{
+    Node *START;
 
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diizinkan\n";
+            }
+        }
+    }
+};
